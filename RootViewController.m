c72076bea7dbe2e6e51617cd008ae43de873c07f
Julian Lee <Julian@Julians-MacBook-Pro.local>
2016-01-18 14:46:58 -0800

    step 5 complete

--- RootViewController.m
+++ RootViewController.m
@@ -9,8 +9,9 @@
 #import "RootViewController.h"
 
 @interface RootViewController () <UITableViewDelegate, UITableViewDataSource>
-
+@property (weak, nonatomic) IBOutlet UITextField *toDoTextField;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property BOOL editModeIsOn;
 
 @property NSMutableArray *toDoArray;
 
@@ -20,6 +21,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.toDoArray = [[NSMutableArray alloc] initWithObjects:@"Code", @"More Code", @"Milk", @"Eggs and code", nil];
+    self.editModeIsOn = false;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,17 +36,63 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier"];
      cell.textLabel.text = [self.toDoArray objectAtIndex:indexPath.row];
+    return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 4;
+    return self.toDoArray.count;
 }
 
-#pragma – other methods
 
-- (IBAction)addToDoTapped:(UIBarButtonItem *)sender {
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     
+    cell.backgroundColor = [UIColor greenColor];
 }
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.toDoArray removeObjectAtIndex:indexPath.row];
+     
+        [tableView reloadData];
+    }
+}
+
+#pragma mark – other methods
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender
+{
+    [self.toDoArray insertObject:self.toDoTextField.text atIndex:0];
+    NSLog(@"%@", self.toDoArray);
+     [self.tableView reloadData];
+    [self.view endEditing:YES];
+    self.toDoTextField.text = nil;
+}
+
+- (IBAction)onEditButtonPressed:(UIBarButtonItem*)sender {
+    if ([sender.title  isEqual: @"Edit"]) {
+        sender.title = @"Done";
+        [self.tableView setEditing:YES animated:YES];
+    }
+    else
+    {
+        sender.title = @"Edit";
+        [self.tableView setEditing:NO animated:NO];
+    }
+}
+
 
 @end
