577fc3938d438729c8207d8396bdf50ce4f84a98
nicholasnaude <nicholas.naude@icloud.com>
2016-01-18 13:41:05 -0800

    New update

--- RootViewController.m
+++ RootViewController.m
@@ -8,7 +8,11 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController ()
+@interface RootViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property NSMutableArray *toDoArray;
 
 @end
 
@@ -16,7 +20,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +27,23 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - TableView methods
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier"];
+     cell.textLabel.text = [self.toDoArray objectAtIndex:indexPath.row];
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+#pragma – other methods
+
+- (IBAction)addToDoTapped:(UIBarButtonItem *)sender {
+    
+}
 
 @end
